cd34cf7a25ec3331ba9527418ea64a32a7875ce1
Chris Adams <chris.ah.adams@gmail.com>
2022-02-27 00:30:03 -0400

    more level gen

--- scripts/object_types/earth.js
+++ scripts/object_types/earth.js
@@ -1,8 +1,8 @@
 window.EarthObject = function () {
   this.objects = [];
-  this.objects.push(new GroundObject(-200, 70));
-  this.objects.push(new WatersObject(70, 80));
-  this.objects.push(new GroundObject(80, 200));
+  this.objects.push(new GroundObject(-200, 200));
+  this.objects.push(new WatersObject(-800, -200));
+  this.objects.push(new WatersObject(200, 800));
 };
 
 EarthObject.prototype.updateRender = function (dt, time, ctx) {
