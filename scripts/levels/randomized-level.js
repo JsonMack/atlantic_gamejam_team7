897ab3931763f92f59b73fe7f3225c830c3c5a71
Jason MacKeigan <w0270109@nscc.ca>
2022-02-27 12:26:38 -0400

    more.

--- scripts/levels/randomized-level.js
+++ scripts/levels/randomized-level.js
@@ -125,7 +125,7 @@ RandomizedLevel.prototype.drawHealthBar = function (
   ctx.fillText(
     currentHealth + '/' + maxHealth,
     healthBarX + healthBarWidth / 2,
-    healthBarY + healthBarHeight - 6
+    healthBarY + healthBarHeight - 8
   );
 
   ctx.textAlign = 'left';
